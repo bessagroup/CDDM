34dd7383105d8ba6be674ebc1d57ca0a4f2c1634
Rui B. Pinto <rbarreira@ethz.ch>
2024-07-31 14:08:41 +0200

    flake8

--- src/cddm/continual_learning.py
+++ src/cddm/continual_learning.py
@@ -1,4 +1,5 @@
-"""This module contains functions that are related to the continual learning procedure.
+"""This module contains functions that are related to
+the continual learning procedure.
 
 
 Functions
@@ -9,12 +10,10 @@ continual_learning
 
 
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
 
 from .train import train
 from .pruning import gru_pruning
-from .utils import *
+from .utils import loss_func, error_func, process_data
 
 
 if torch.cuda.is_available():
@@ -23,54 +22,72 @@ else:
     device = torch.device('cpu')
 
 
-
-def continual_learning(net, file_names, alpha, optimizer_name, scheduler, n_epochs, lr, weight_decay, 
-                       device, nums_train=[800, 100, 100, 100], num_val=100, num_test=100,
-                       SCALE=False, seed=0, path_to_save="model.pth", result_folder='./result', problem='plasticity-plates'):
+def continual_learning(net, file_names, alpha, optimizer_name,
+                       scheduler, n_epochs, lr, weight_decay,
+                       device, nums_train=[800, 100, 100, 100],
+                       num_val=100, num_test=100,
+                       SCALE=False, seed=0, path_to_save="model.pth",
+                       result_folder='./result', problem='plasticity-plates'):
     """ A function for continual learning of the given sequence of tasks.
-    
     Parameters
     ----------
+
     net : PyTorch model
-        The learnable GRU model.
+    The learnable GRU model.
+
     file_names : list
-        The list of input files.
+    The list of input files.
+
     alpha : float
-        Pruning parameter between 0 and 1. The higher value the less aggressive the pruning.
+    Pruning parameter between 0 and 1.
+    The higher value the less aggressive the pruning.
+
     optimizer_name : str
-        Optimizer name.
+    Optimizer name.
+
     scheduler : torch.optim.lr_scheduler
-        Scheduler for learning rate planning.
+    Scheduler for learning rate planning.
+
     n_epochs : int
-        The number of training epochs.
+    The number of training epochs.
+
     lr : float
-        Learning rate.
+    Learning rate.
+
     weight_decay : float
-        Weight decay.    
+    Weight decay.
+
     device: torch.device ('cpu' or 'cuda')
-        The device on which PyTorch model and all torch.Tensor are or will be allocated.
+    The device on which PyTorch model and all torch.
+    Tensor are or will be allocated.
+
     nums_train : list
-        List of the number of training paths for each task.
+    List of the number of training paths for each task.
+
     num_val, num_test : int, int
-        The number of validation/test paths.
-    SCALE : bool 
-        Scale data or not.
-    seed : int 
-        Random seed.
+    The number of validation/test paths.
+
+    SCALE : bool
+    Scale data or not.
+
+    seed : int
+    Random seed.
+
     path_to_save : str
-        Paths for saving the model.
+    Paths for saving the model.
+
     result_folder : str
-        Path for result folder.    
+    Path for result folder.
+
     problem : str
-        Type of the problem    
-        
+    Type of the problem
+
     Returns
     -------
     net : PyTorch model
-        The network where FC layer is pruned for the task number task_id.    
-    """   
-    
-    
+    The network where FC layer is pruned for the task number task_id.
+    """
+
     num_tasks = len(file_names)
     print(file_names)
 
@@ -81,46 +98,47 @@ def continual_learning(net, file_names, alpha, optimizer_name, scheduler, n_epoc
         if nums_train[task_id] < 1:
             print('ERROR: Number of training paths must be positive')
             return
-    
+
     for task_id in range(num_tasks):
         num_train = nums_train[task_id]
 
         print('TRAIN PATHS: ', num_train)
-        
-        x_train, y_train, x_val, y_val, x_test, y_test, x_mean, x_std, y_mean, y_std = process_data(file_names[task_id], 
-                                                                                                    num_train=num_train, 
-                                                                                                    num_val=num_val, 
-                                                                                                    num_test=num_test,
-                                                                                                    problem=problem
-                                                                                                   )
-      
-        print('-------------------TASK {}------------------------------'.format(task_id+1)) 
-        
-        
+
+        x_train, y_train, x_val, y_val, x_test, y_test, _, _, y_mean, y_std = process_data(file_names[task_id],
+                                                                                           num_train=num_train,
+                                                                                           num_val=num_val,
+                                                                                           num_test=num_test,
+                                                                                           problem=problem)
+
+        print('--------------TASK {}---------------------'.format(task_id+1))
+
         if optimizer_name.lower() == 'adam':
-            optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
+            optimizer = torch.optim.Adam(net.parameters(),
+                                         lr=lr, weight_decay=weight_decay)
         else:
-            optimizer = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=weight_decay)
-        
+            optimizer = torch.optim.SGD(net.parameters(),
+                                        lr=lr, weight_decay=weight_decay)
+
         net.set_task(task_id)
         net.set_trainable_masks(task_id)
 
-        net = train(net=net, 
+        net = train(net=net,
                     x_train=x_train, y_train=y_train,
                     x_val=x_val, y_val=y_val,
                     device=device,
-                    lr = lr, n_epochs=n_epochs, 
+                    lr=lr, n_epochs=n_epochs,
                     optimizer=optimizer, scheduler=scheduler,
                     task_id=task_id,
-                    path_to_save=path_to_save
-                   )
+                    path_to_save=path_to_save)
 
         net.load_state_dict(torch.load(f"{result_folder}/{path_to_save}"))
-        
+
         net.eval()
         y_pred = net(x_test)
         print("test loss: ", loss_func(y_test, y_pred).item())
-        print("error: %.3f" % (100*error_func(y_test*y_std + y_mean, y_pred*y_std + y_mean).item()) +"%"  )
+        print("error: %.3f" %
+              (100*error_func(y_test*y_std + y_mean, y_pred*y_std +
+                              y_mean).item()) + "%")
         print("------------------")
 
         if num_tasks > 1:
@@ -128,32 +146,33 @@ def continual_learning(net, file_names, alpha, optimizer_name, scheduler, n_epoc
 
             net.set_trainable_masks(task_id)
 
-            optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=1e-6)
-            net = train(net=net, 
+            optimizer = torch.optim.Adam(net.parameters(),
+                                         lr=lr, weight_decay=1e-6)
+            net = train(net=net,
                         x_train=x_train, y_train=y_train,
                         x_val=x_val, y_val=y_val,
                         device=device,
-                        lr = lr, n_epochs=n_epochs//5, 
+                        lr=lr, n_epochs=n_epochs//5,
                         optimizer=optimizer, scheduler=scheduler,
                         task_id=task_id,
-                        path_to_save=path_to_save
-                       )
+                        path_to_save=path_to_save)
 
             net.load_state_dict(torch.load(f"{result_folder}/{path_to_save}"))
 
             net.eval()
             y_pred = net(x_test)
             print("test loss: ", loss_func(y_test, y_pred).item())
-            print("error: %.3f" % (100*error_func(y_test*y_std + y_mean, y_pred*y_std + y_mean).item()) +"%"  )
+            print("error: %.3f" %
+                  (100*error_func(y_test*y_std + y_mean, y_pred*y_std +
+                                  y_mean).item()) + "%")
             print("------------------")
 
             net.set_masks_intersection()
             net.set_masks_union()
 
             net.save_masks(f"{result_folder}/masks_" + path_to_save)
-            
 
             if task_id + 1 < num_tasks:
                 net.add_mask(task_id=task_id+1)
-    
-    return net         
+
+    return net
--- src/cddm/main.py
+++ src/cddm/main.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import random
 
-from .utils import *
-from .continual_learning import *
+from .utils import gru_total_params_mask, set_seed
+from .continual_learning import continual_learning
 from .models import GRU
 
 import argparse
@@ -20,7 +20,7 @@ else:
 
 
 def main():
-    
+
     ################
     # Arguments
     ################
@@ -31,47 +31,47 @@ def main():
     parser.add_argument('--tasks', type=str, default='A,B,C,D', help='Tasks to learn.')
     parser.add_argument('--nums_train', type=str, default='800, 100, 100, 100', help='Number of training paths.')
     parser.add_argument('--data_folder', type=str, default='./data/plates', help='Path to folder with data.')
-    
+
     parser.add_argument('--input_size', type=int, default=3, help='Number of input neurons.')
     parser.add_argument('--output_size', type=int, default=3, help='Number of output neurons.')
     parser.add_argument('--num_grucells', type=int, default=2, help='Number of GRU cells.')
-    parser.add_argument('--hidden_size', type=int, default=128, help='Number of features in the hidden state.')    
+    parser.add_argument('--hidden_size', type=int, default=128, help='Number of features in the hidden state.')
     parser.add_argument('--seq_len', type=int, default=101, help='Data sequence length.')
-    
+
     parser.add_argument('--optimizer_name', type=str, default='Adam', help='Optimizer of choice.')
     parser.add_argument('--lr', type=float, default=1e-2, help='Learning rate.')
     parser.add_argument('--weight_decay', type=float, default=1e-6, help='Weight decay.')
     parser.add_argument('--n_epochs', type=int, default=1000, help='Number of training epochs.')
     parser.add_argument('--alpha', type=float, default=0.95, help='Pruning parameter')
-    
+
     parser.add_argument('--save_model', action='store_true', help='Save the model.')
     parser.add_argument('--save_result', action='store_true', help='Save the results.')
     parser.add_argument('--result_folder', type=str, default='./result', help='Path to save the results.')
-    
+
     parser.add_argument('--seed', type=int, default=0, help='Random initialization.')
 
-    args = parser.parse_args()   
-    
+    args = parser.parse_args()
+
     problem = args.problem
     model_name = args.model_name
     data_folder = args.data_folder
     result_folder = args.result_folder
-    
+
     tasks = [task.strip() for task in args.tasks.split(',')]
-    
+
     if problem == 'plasticity-plates':
         file_names = [f'{data_folder}/{task}.pkl' for task in tasks]
     else:
         file_names = [f'{data_folder}/{task}.pickle' for task in tasks]
-        
-    num_tasks = len(file_names)
-    
+
+    # num_tasks = len(file_names)
+
     SAVE_MODELS = args.save_model
     SAVE_DATA = args.save_result
 
     input_size = args.input_size
     output_size = args.output_size
-    seq_len = args.seq_len          
+    seq_len = args.seq_len
     hidden_size = args.hidden_size
     num_layers = args.num_grucells
 
@@ -90,96 +90,87 @@ def main():
     all_errors = {}
 
     nums_train = [int(num_train.strip()) for num_train in args.nums_train.split(',')]
-    
-    if not os.path.exists(result_folder):
-            os.mkdir(result_folder)
 
-  
+    if not os.path.exists(result_folder):
+        os.mkdir(result_folder)
+
     print(f"################## SEED {seed} ##################")
     set_seed(seed)
 
-    #for num_train in nums_train:  
-    net = GRU(input_size=input_size, seq_len=seq_len, hidden_size=hidden_size, 
+    # for num_train in nums_train:
+    net = GRU(input_size=input_size, seq_len=seq_len, hidden_size=hidden_size,
               num_layers=num_layers, output_size=output_size, device=device).to(device)
-            
 
     print('Total params: ', gru_total_params_mask(net))
 
     if SAVE_MODELS:
         if not os.path.exists(result_folder):
             os.mkdir(result_folder)
-            
+
         dict_names = {}
         case_name = f'{problem}_'
         for i, task in enumerate(tasks):
             case_name += f'{task}-'
 
-        case_name = case_name[:-1] 
+        case_name = case_name[:-1]
 
         path_to_save = f"{model_name}_{case_name}_seed{seed}_num-layers{num_layers}_hidden{hidden_size}_lr0.01_alpha{alpha}.pth"
     else:
         path_to_save = f"model.pth"
 
-
     print(f">>>>>>>>>>>>>>>>>>>{nums_train} TRAINING POINTS<<<<<<<<<<<<<<<<<<<<")
-            
-                           
-    net = continual_learning(net, 
-                             file_names=file_names, alpha=alpha, 
+
+    net = continual_learning(net,
+                             file_names=file_names, alpha=alpha,
                              optimizer_name=optimizer_name, scheduler=None,
-                             n_epochs=n_epochs, lr=lr, weight_decay=weight_decay, 
-                             device=device, 
+                             n_epochs=n_epochs, lr=lr, weight_decay=weight_decay,
+                             device=device,
                              nums_train=nums_train,
                              num_val=num_val,
                              num_test=num_test,
                              seed=seed,
                              path_to_save=path_to_save,
                              result_folder=result_folder,
-                             problem=problem
-                            )                
-                
+                             problem=problem)
+
     losses, errors = eval(net, file_names, nums_train=nums_train, num_val=num_val, num_test=num_test, problem=problem)
-         
-    
+
     for i in range(len(nums_train)):
         all_losses[f"{tasks[i]}_{nums_train[i]}points"] = losses[i]
         all_errors[f"{tasks[i]}_{nums_train[i]}points"] = errors[i]
 
-    
-    df_losses = pd.DataFrame([all_losses])
+    # df_losses = pd.DataFrame([all_losses])
     df_errors = pd.DataFrame([all_errors])
-    
-    print(df_errors)
 
+    print(df_errors)
 
     if SAVE_DATA:
         if not os.path.exists(result_folder):
             os.mkdir(result_folder)
-            
+
         dict_names = {}
         case_name = f'{problem}_'
         for i, task in enumerate(tasks):
             dict_names[i] = f'{task}'
             case_name += f'{task}-'
 
-        case_name = case_name[:-1]    
-        df_errors.rename(index = dict_names, inplace = True)
-        
+        case_name = case_name[:-1]
+        df_errors.rename(index=dict_names, inplace=True)
+
         if len(tasks) > 1:
-            df_errors.to_csv(f"{result_folder}/{case_name}_error_seed{seed}_num-layers{num_layers}_hidden{hidden_size}_lr{lr}_alpha{alpha}.csv", index=False)
+            df_errors.to_csv(f"{result_folder}/{case_name}_error_seed{seed}_num-layers{num_layers}_hidden{hidden_size}_lr{lr}_alpha{alpha}.csv",
+                             index=False)
         else:
-            df_errors.to_csv(f"{result_folder}/{case_name}_error_seed{seed}_num-layers{num_layers}_hidden{hidden_size}_lr{lr}.csv", index=False)
-            
+            df_errors.to_csv(f"{result_folder}/{case_name}_error_seed{seed}_num-layers{num_layers}_hidden{hidden_size}_lr{lr}.csv",
+                             index=False)
+
     if os.path.exists(f"{result_folder}/model.pth"):
-          os.remove(f"{result_folder}/model.pth") 
-            
-    
+        os.remove(f"{result_folder}/model.pth")
+
     if os.path.exists(f"{result_folder}/masks_model.pth"):
-          os.remove(f"{result_folder}/masks_model.pth")    
-            
+        os.remove(f"{result_folder}/masks_model.pth")
+
     return
 
-
-                
 if __name__ == "__main__":
     main()
